562bcbd46b463392d15bce3a154a7a76c2acd569
jli <jeffvictorli@gmail.com>
2019-03-25 14:52:52 -0700

    Update

--- Dashboard.ps1
+++ Dashboard.ps1
@@ -243,23 +243,27 @@ $AccountPage = New-UDPage -Name "Accounts" -Icon home -Content {
 }
 
 $LicensePage = New-UDPage -Name "Licenses" -Content {
-    New-UDChart -Type Bar -AutoRefresh -RefreshInterval 300 -Endpoint {
-        $Cache:SortedLicenses | Out-UDChartData -LabelProperty 'License' -DataSet @(
-            New-UDChartDataset -DataProperty 'UsedLicenses' -Label 'Assigned Licenses' -BackgroundColor '#80962F23' -HoverBackgroundColor '#80962F23'
-            New-UDChartDataset -DataProperty 'AvailableLicenses' -Label 'Available Licenses' -BackgroundColor '#8014558C' -HoverBackgroundColor '#8014558C'
-        )
-    } -Labels @('Licenses') -Options @{
-        scales = @{
-            xAxes = @(
-                @{
-                    stacked = $true
+    New-UDRow -Columns {
+        New-UDColumn -Size 12 {
+            New-UDChart -Type Bar -AutoRefresh -RefreshInterval 300 -Endpoint {
+                $Cache:SortedLicenses | Out-UDChartData -LabelProperty 'License' -DataSet @(
+                    New-UDChartDataset -DataProperty 'UsedLicenses' -Label 'Assigned Licenses' -BackgroundColor '#80962F23' -HoverBackgroundColor '#80962F23'
+                    New-UDChartDataset -DataProperty 'AvailableLicenses' -Label 'Available Licenses' -BackgroundColor '#8014558C' -HoverBackgroundColor '#8014558C'
+                )
+            } -Labels @('Licenses') -Options @{
+                scales = @{
+                    xAxes = @(
+                        @{
+                            stacked = $true
+                        }
+                    )
+                    yAxes = @(
+                        @{
+                            stacked = $true
+                        }
+                    )
                 }
-            )
-            yAxes = @(
-                @{
-                    stacked = $true
-                }
-            )
+            }
         }
     }
 <#
